abf81764356177a551513cd51b73f114020775eb
miketillman95 <matillman95@gmail.com>
2020-10-18 22:22:58 -0700

    hmwk

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+var p = 2000;
+var I = .05;
+var N = 30;
 
 
 
@@ -14,7 +17,8 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
+var monthlyInterestRate = .0041
+var periods = 360
 
 
 
@@ -22,6 +26,7 @@
 /* Create your calculator! Use the formula in the ReadMe (also below) to run calculations on your numbers. Save the final value into a variable called monthlyRate. 
 
 M = P [ I ( 1 + I )^N ] / [ ( 1 + I )^N – 1 ]
+
 
 Hint: while these calculations can be done in one line, it might be helpful to create seperate variables to hold parts of your equation. That might look like this:
 
@@ -34,6 +39,8 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+console.log(P  I ( 1 + I )^N  /  ( 1 + I )^N – 1 );
+monthlyInterestRate 
 
 
 
